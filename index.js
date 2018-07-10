218a2dba81732407d670f22d80d873ec6e2a4c8c
Janet Mardjuki <mardjuki@sfu.ca>
2018-07-09 20:37:18 -0700

    Added commands

--- index.js
+++ index.js
@@ -1,90 +1,77 @@
+const fs = require('fs');
 const Discord = require('discord.js');
-
 const { prefix, token } = require('./config.json');
 
 const client = new Discord.Client();
+
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+const cooldowns = new Discord.Collection();
 
 client.on('ready', () => {
   console.log('Ready!');
 });
 
 client.on('message', message => {
-  console.log(message.content);
-  // Return early if no prefix
-  if (!message.content.startsWith(prefix) || message.author.bot) {
-    return;
-  }
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
   const args = message.content.slice(prefix.length).split(/ +/);
-  const command = args.shift().toLowerCase();
+  const commandName = args.shift().toLowerCase();
 
-  if (command === 'args-info') {
-    if (!args.length) {
-      return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+  const command = client.commands.get(commandName)
+    || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+  if (!command) return;
+  if (command.guildOnly && message.channel.type !== 'text') {
+    return message.reply('I can\'t execute that command inside DMs!');
+  }
+
+  if (command.args && !args.length) {
+    let reply = `You didn't provide any arguments, ${message.author}!`;
+    if (command.usage) {
+      reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
     }
-    else if (args[0] === 'foo') {
-      return message.channel.send('bar');
+    return message.channel.send(reply);
+  }
+
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Discord.Collection());
+  }
+
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 3) * 1000;
+
+  if (!timestamps.has(message.author.id)) {
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+  }
+  else {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
     }
 
-    message.channel.send(`First argument: ${args[0]}`);
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
   }
-  else if (command === 'kick') {
-    // Early return if nobody is tagged
-    if (!message.mentions.users.size) {
-      return message.reply('you need to tag a user in order to kick them!');
-    }
-    const taggedUser = message.mentions.users.first();
 
-    message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+  try {
+    command.execute(message, args);
   }
-  else if (command === 'avatar') {
-    if (!message.mentions.users.size) {
-      return message.channel.send(`Your avatar: ${message.author.displayAvatarURL}`);
-    }
-
-    const avatarList = message.mentions.users.map(user => {
-      return `${user.username}'s avatar: ${user.displayAvatarURL}`;
-    });
-
-      // send the entire array of strings as a message
-      // by default, discord.js will `.join()` the array with `\n`
-    message.channel.send(avatarList);
+  catch (error) {
+    console.error(error);
+    message.reply('there was an error trying to execute that command!');
   }
-  else if (command === 'prune') {
-    // Bad fix, cause delete also add current
-    const amount = parseInt(args[0]) + 1;
-
-    if (isNaN(amount)) {
-      return message.reply('that doesn\'t seem to be a valid number.');
-    }
-    else if (amount <= 1 || amount > 100) {
-      return message.reply('you need to input a number between 1 and 99.');
-    }
-    message.channel.bulkDelete(amount, true).catch(err => {
-      console.error(err);
-      message.channel.send('there was an error trying to prune messages in this channel!');
-    });
-
-  }
-  /*
-  if (message.content === `${prefix}ping`) {
-    message.channel.send('Pong.');
-  }
-  else if (message.content === `${prefix}beep`) {
-    message.channel.send('Boop.');
-  }
-  // Fix this fotmating later
-  else if (message.content === `${prefix}serverinfo`) {
-    message.channel.send(`Server name: ${message.guild.name}
-      \nTotal members: ${message.guild.memberCount}
-      \nCreated At: ${message.guild.createdAt}
-      \nRegion: ${message.guild.region}`
-    );
-  }
-  else if (message.content === `${prefix}user-info`) {
-      message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-  }
-  */
+  // do the same for the rest of the commands...
 });
 
 client.login(token);
